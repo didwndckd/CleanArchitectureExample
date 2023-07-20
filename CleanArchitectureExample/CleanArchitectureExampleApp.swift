52c2050fb497641af03a9423cfdb8710faa891fd
Colin <colin@kmong.com>
2023-07-20 12:01:32 +0900

    Initial Commit

---
+++ CleanArchitectureExample/CleanArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureExampleApp.swift
+//  CleanArchitectureExample
+//
+//  Created by yjc on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
